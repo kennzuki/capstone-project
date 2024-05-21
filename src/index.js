64d3aad983fde39d29daf2fbdb62cb2bdc27593f
kennzuki <kingsyken@gmail.com>
2024-05-21 15:51:54 +0300

    getting html elements and form

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 /** This is the entry point of your app */
+const form = document.querySelector("input-container");
+
+const card = document.getElementById('card');
+const randomImg = document.getElementById('random-img');
+const recipeName = document.getElementById('recipe-name');
+const recipeIngredients = document.getElementById('recipe-ingredients');
+
+
+
 
