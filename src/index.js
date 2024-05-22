c4d63df0cc4987a7d2435bfb3961d7273e914442
kennzuki <kingsyken@gmail.com>
2024-05-22 22:38:55 +0300

    changed random function to get eight random meals at a time using for loop

--- src/index.js
+++ src/index.js
@@ -2,37 +2,33 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 /** This is the entry point of your app */
-const mealEl = document.getElementById('meals');
-getRandomMeal();
-
 async function getRandomMeal() {
-  const resp = await fetch(
-    'https://www.themealdb.com/api/json/v1/1/random.php'
-  );
-  const respData = await resp.json();
-  const randomMeal = respData.meals[0];
+  try {
+    const cardsContainer = document.getElementById('cards-container');
+    for (let i = 0; i < 8; i++) {
+      // Fetch a random meal
+      let response = await fetch(
+        'https://www.themealdb.com/api/json/v1/1/random.php'
+      );
+      let data = await response.json();
+      let meal = data.meals[0]; // Get the meal object
 
-  addMeal(randomMeal);
+      // Create a card for the meal
+      const card = document.createElement('div');
+      card.className = 'card';
+      card.innerHTML = `
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                    <h2>${meal.strMeal}</h2>
+            
+                `;
+
+      // Append the card to the container
+      cardsContainer.appendChild(card);
+    }
+  } catch (error) {
+    //console.error('Error fetching data:', error);
+  }
 }
 
-function addMeal(mealData) {
-  console.log(mealData);
-
-  const meal = document.createElement('div');
-  meal.classList.add('meals');
-
-  meal.innerHTML = `
-  <div >
-  <img
-  id="random-img"
-  src="${mealData.strMealThumb}"
-  alt="${mealData.strMeal}"
-    class="w-[200px]"
-  />
-  <h4 id="recipe-name" class="">${mealData.strMeal}</h4>
-  <button id="fav-button" class="fav">favorite</button>
-  <button id="view-btn" class="view">view recipe</button>
-</div>
-`;
-  mealEl.appendChild(meal);
-}
+// Initialize
+getRandomMeal();
