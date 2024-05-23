f959c1fc1aa52e825e4efb917b8958cec1499cd8
kennzuki <kingsyken@gmail.com>
2024-05-23 22:42:27 +0300

    refactor to class using meal.js

--- src/Meal.js
+++ src/Meal.js
@@ -1,15 +1,8 @@
-export default class MealDB {
-  constructor() {
-    this.baseUrl = 'https://www.themealdb.com/api/json/v1/1/';
+export class MealDB {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+    this.baseUrl = 'https://www.themealdb.com/api/json/v1/1';
   }
 
-  async getRandomMeals() {
-    try {
-      const res = fetch(`${this.baseUrl}random.php`);
-      const data = (await res).json();
-      return data.meals[0];
-    } catch (error) {
-      return error;
-    }
-  }
+
 }
