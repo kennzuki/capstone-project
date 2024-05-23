85f8e797b96013dd6b6dbe70cbd9313971f775ab
kennzuki <kingsyken@gmail.com>
2024-05-23 22:44:48 +0300

    fetch meals by name method

--- src/Meal.js
+++ src/Meal.js
@@ -4,5 +4,14 @@ export class MealDB {
     this.baseUrl = 'https://www.themealdb.com/api/json/v1/1';
   }
 
-
+  async fetchMealsByName(name) {
+    try {
+      const response = await fetch(`${this.baseUrl}/search.php?s=${name}`);
+      const data = await response.json();
+      return data.meals;
+    } catch (error) {
+      return error;
+      
+    }
+  }
 }
